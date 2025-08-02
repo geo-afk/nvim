e03c03dbdfcd694ff260d54b088f0337946e656e
geo-afk <geo.a.stewart21@gmail.com>
2025-08-01 19:23:57 -0500

    added word
    added custom word for the typos_lsp server

--- lua/config/lsp/servers/typos.lua
+++ lua/config/lsp/servers/typos.lua
@@ -3,9 +3,7 @@ local handlers = require("config.lsp.handlers")
 return {
   capabilities = handlers.get_capabilities(),
   on_attach = handlers.on_attach,
-  settings = {
-    typos = {
-      config = "../../../../typos.toml",
-    },
+  init_options = {
+    config = vim.fn.expand("~/AppData/Local/nvim/typos.toml"),
   },
 }
