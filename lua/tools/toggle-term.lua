dc1b953d81e88c640aeb4e8d4528e76358b97a1f
geo-afk <geo.a.stewart21@gmail.com>
2025-10-06 13:28:09 -0500

    added scooter opening

--- lua/tools/toggle-term.lua
+++ lua/tools/toggle-term.lua
@@ -57,6 +57,69 @@ return {
         end
       end
 
+      -- ========== SCOOTER INTEGRATION ==========
+      local scooter_term = nil
+
+      -- Open existing scooter terminal if one is available, otherwise create a new one
+      local function open_scooter()
+        if not scooter_term then
+          scooter_term = Terminal:new {
+            cmd = 'scooter',
+            direction = 'float',
+            close_on_exit = true,
+            on_open = function()
+              vim.cmd 'startinsert!'
+            end,
+            on_exit = function()
+              scooter_term = nil
+            end,
+          }
+        end
+        scooter_term:open()
+      end
+
+      -- Called by scooter to open the selected file at the correct line from the scooter search list
+      _G.EditLineFromScooter = function(file_path, line)
+        if scooter_term and scooter_term:is_open() then
+          scooter_term:close()
+        end
+
+        local current_path = vim.fn.expand '%:p'
+        local target_path = vim.fn.fnamemodify(file_path, ':p')
+
+        if current_path ~= target_path then
+          vim.cmd.edit(vim.fn.fnameescape(file_path))
+        end
+
+        vim.api.nvim_win_set_cursor(0, { line, 0 })
+      end
+
+      -- Opens scooter with the search text populated by the `search_text` arg
+      _G.OpenScooterSearchText = function(search_text)
+        if scooter_term and scooter_term:is_open() then
+          scooter_term:close()
+        end
+
+        local escaped_text = vim.fn.shellescape(search_text:gsub('\r?\n', ' '))
+        scooter_term = Terminal:new {
+          cmd = 'scooter --search-text ' .. escaped_text,
+          direction = 'float',
+          close_on_exit = true,
+          on_open = function()
+            vim.cmd 'startinsert!'
+          end,
+          on_exit = function()
+            scooter_term = nil
+          end,
+        }
+        scooter_term:open()
+      end
+
+      -- Scooter keymaps
+      vim.keymap.set('n', '<leader>ts', open_scooter, { desc = 'Open scooter' })
+      vim.keymap.set('v', '<leader>tr', '"ay<ESC><cmd>lua OpenScooterSearchText(vim.fn.getreg("a"))<CR>', { desc = 'Search selected text in scooter' })
+
+      -- ========== TERMINAL NAVIGATION ==========
       -- Terminal navigation keymaps
       local function set_terminal_keymaps()
         local term_opts = { buffer = 0, noremap = true, silent = true }
@@ -78,21 +141,26 @@ return {
         end,
       })
 
+      -- ========== WHICH-KEY REGISTRATION ==========
       -- Register which-key mappings
-      local wk = require 'which-key'
-      local mappings = {
-        { '<leader>t', group = 'Terminal', icon = '' },
-        { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = 'Toggle Default Terminal' },
-        { '<leader>t1', '<cmd>1ToggleTerm<cr>', desc = 'Terminal 1' },
-      }
-      for i = 2, terminal_count do
-        table.insert(mappings, {
-          '<leader>t' .. i,
-          '<cmd>lua _TERMINAL_' .. i .. '_TOGGLE()<cr>',
-          desc = 'Terminal ' .. i,
-        })
+      local ok, wk = pcall(require, 'which-key')
+      if ok then
+        local mappings = {
+          { '<leader>t', group = 'Terminal', icon = '' },
+          { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = 'Toggle Default Terminal' },
+          { '<leader>t1', '<cmd>1ToggleTerm<cr>', desc = 'Terminal 1' },
+          { '<leader>ts', desc = 'Open scooter' },
+          { '<leader>tr', desc = 'Search selected text in scooter', mode = 'v' },
+        }
+        for i = 2, terminal_count do
+          table.insert(mappings, {
+            '<leader>t' .. i,
+            '<cmd>lua _TERMINAL_' .. i .. '_TOGGLE()<cr>',
+            desc = 'Terminal ' .. i,
+          })
+        end
+        wk.add(mappings)
       end
-      wk.add(mappings)
     end,
   },
 }
