bc64fdd7f7d35e35a37c8d130456711c7162dde0
geo-afk <geo.a.stewart21@gmail.com>
2025-10-06 13:08:15 -0500

    removed old diagnostic config

--- lua/constants/lsp_util.lua
+++ lua/constants/lsp_util.lua
@@ -47,54 +47,6 @@ M.diagnostic = {
   },
 }
 
--- M.diagnostic = {
---   severity_sort = true,
---   float = {
---     focusable = false,
---     style = 'minimal',
---     border = 'rounded',
---     source = 'is_many',
---     header = '',
---     prefix = function(diagnostic)
---       local level = vim.diagnostic.severity[diagnostic.severity]
---       local prefix = (' %s '):format(diagnostic_icons[level])
---       return prefix, 'Diagnostic' .. level:gsub('^%l', string.upper)
---     end,
---   },
---   underline = { severity = vim.diagnostic.severity.ERROR },
---   update_in_insert = false,
---   signs = vim.g.have_nerd_font and {
---     text = {
---       [vim.diagnostic.severity.ERROR] = '󰅚 ',
---       [vim.diagnostic.severity.WARN] = '󰀪 ',
---       [vim.diagnostic.severity.INFO] = '󰋽 ',
---       [vim.diagnostic.severity.HINT] = diagnostic_icons.HINT,
---     },
---   } or {},
---   -- virtual_text = {
---   --   prefix = '',
---   --   format = function(diagnostic)
---   --     local icon = diagnostic_icons[vim.diagnostic.severity[diagnostic.severity]] --[[@as string]]
---   --     local message = vim.split(diagnostic.message, '\n')[1]
---   --     return ('%s %s '):format(icon, message)
---   --   end,
---   -- },
---   virtual_text = {
---     source = 'if_many',
---     spacing = 2,
---     prefix = '●',
---     format = function(diagnostic)
---       local diagnostic_message = {
---         [vim.diagnostic.severity.ERROR] = diagnostic.message,
---         [vim.diagnostic.severity.WARN] = diagnostic.message,
---         [vim.diagnostic.severity.INFO] = diagnostic.message,
---         [vim.diagnostic.severity.HINT] = diagnostic.message,
---       }
---       return diagnostic_message[diagnostic.severity]
---     end,
---   },
--- }
-
 M.mason_tool_install = {
   -- Go tools
   'gofumpt',
