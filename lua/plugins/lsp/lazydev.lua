617881393b903ff1425bbb3be5826c4c735c3a78
geo-afk <geo.a.stewart21@gmail.com>
2026-03-21 11:15:24 -0500

    added libraries

--- lua/plugins/lsp/lazydev.lua
+++ lua/plugins/lsp/lazydev.lua
@@ -1,17 +1,19 @@
 return {
   {
-    'folke/lazydev.nvim',
-    ft = 'lua', -- only load on lua files
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
     opts = {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         { path = "nvim-treesitter", mods = { "nvim-treesitter" } },
         { path = "mason.nvim", mods = { "mason", "mason-core", "mason-registry", "mason-vendor" } },
         { path = "lazydev.nvim", mods = { "" } },
         { path = "LuaSnip", mods = { "luasnip" } },
+        { path = "nvim-lspconfig", mods = { "lspconfig" } },
         { path = "friendly-snippets", mods = { "snippets" } }, -- has vimscript
+        { "~/AppData/Local/nvim-data/lazy/blink.cmp/lua/" },
         -- {
         --   path = 'lazy.nvim',
         --   files = vim.tbl_map(function(file)
