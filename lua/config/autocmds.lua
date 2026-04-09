ce146b386286f57488b8ffdb34acbd92af970862
geo-afk <geo.a.stewart21@gmail.com>
2026-04-08 23:59:39 -0500

    add autocmd for file saving, etc

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -101,36 +101,36 @@ autocmd("FileType", {
 --------------------------------------------------------------------------------
 -- AUTO-SAVE
 --------------------------------------------------------------------------------
-autocmd({ "InsertLeave", "TextChanged", "BufLeave", "FocusLost" }, {
-  desc = "User: Auto-save",
-  callback = function(ctx)
-    local saveInstantly = ctx.event == "FocusLost" or ctx.event == "BufLeave"
-    local bufnr = ctx.buf
-    local bo, b = vim.bo[bufnr], vim.b[bufnr]
-    local bufname = ctx.file
-    if bo.buftype ~= "" or bo.ft == "gitcommit" or bo.readonly then
-      return
-    end
-    if b.saveQueued and not saveInstantly then
-      return
-    end
-
-    b.saveQueued = true
-    vim.defer_fn(function()
-      if not vim.api.nvim_buf_is_valid(bufnr) then
-        return
-      end
-
-      vim.api.nvim_buf_call(bufnr, function()
-        -- saving with explicit name prevents issues when changing `cwd`
-        -- `:update!` suppresses "The file has been changed since reading it!!!"
-        local vimCmd = ("silent! noautocmd lockmarks update! %q"):format(bufname)
-        vim.cmd(vimCmd)
-      end)
-      b.saveQueued = false
-    end, saveInstantly and 0 or 2000)
-  end,
-})
+-- autocmd({ "InsertLeave", "TextChanged", "BufLeave", "FocusLost" }, {
+--   desc = "User: Auto-save",
+--   callback = function(ctx)
+--     local saveInstantly = ctx.event == "FocusLost" or ctx.event == "BufLeave"
+--     local bufnr = ctx.buf
+--     local bo, b = vim.bo[bufnr], vim.b[bufnr]
+--     local bufname = ctx.file
+--     if bo.buftype ~= "" or bo.ft == "gitcommit" or bo.readonly then
+--       return
+--     end
+--     if b.saveQueued and not saveInstantly then
+--       return
+--     end
+--
+--     b.saveQueued = true
+--     vim.defer_fn(function()
+--       if not vim.api.nvim_buf_is_valid(bufnr) then
+--         return
+--       end
+--
+--       vim.api.nvim_buf_call(bufnr, function()
+--         -- saving with explicit name prevents issues when changing `cwd`
+--         -- `:update!` suppresses "The file has been changed since reading it!!!"
+--         local vimCmd = ("silent! noautocmd lockmarks update! %q"):format(bufname)
+--         vim.cmd(vimCmd)
+--       end)
+--       b.saveQueued = false
+--     end, saveInstantly and 0 or 2000)
+--   end,
+-- })
 
 -- =============================================================================
 --  MARK SET  (0.12-new)
