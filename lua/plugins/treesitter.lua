516f285156f6c91b2861f76d7b9459fb3917cd0c
geo-afk <geo.a.stewart21@gmail.com>
2026-04-09 00:00:08 -0500

    formatted by conform

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,84 +6,101 @@ local utils_ok, utils = pcall(require, "utils")
 utils = utils_ok and utils or {}
 
 vim.pack.add({
-	{
-		src = "https://github.com/nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-	},
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
 })
 
 local ok, ts = pcall(require, "nvim-treesitter.configs")
 if not ok then
-	return
+  return
 end
 
 ts.setup({
-	ensure_installed = {
-		"lua",
-		"typescript",
-		"tsx",
-		"javascript",
-		"go",
-		"json",
-		"jsonc",
-		"html",
-		"css",
-		"scss",
-		"markdown",
-		"markdown_inline",
-		"regex",
-		"vim",
-		"vimdoc",
-		"query",
-		"toml",
-		"sql",
-		"angular",
-	},
-	auto_install = true,
-	highlight = { enable = true, additional_vim_regex_highlighting = false },
-	indent = { enable = true, disable = { "ruby" } },
+  ensure_installed = {
+    "lua",
+    "typescript",
+    "tsx",
+    "javascript",
+    "go",
+    "json",
+    "jsonc",
+    "html",
+    "css",
+    "scss",
+    "markdown",
+    "markdown_inline",
+    "regex",
+    "vim",
+    "vimdoc",
+    "query",
+    "toml",
+    "sql",
+    "angular",
+  },
+  auto_install = true,
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
+  indent = { enable = true, disable = { "ruby" } },
 
-	-- [0.12] incremental_selection also powered by LSP selectionRange (v_an/v_in)
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "gnn",
-			node_incremental = "grn",
-			scope_incremental = "grc",
-			node_decremental = "grm",
-		},
-	},
+  -- [0.12] incremental_selection also powered by LSP selectionRange (v_an/v_in)
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 })
 local function is_angular_project()
-	if type(utils.is_angular_project) == "function" then
-		return utils.is_angular_project()
-	end
-	return false
+  if type(utils.is_angular_project) == "function" then
+    return utils.is_angular_project()
+  end
+  return false
 end
 
 local function should_use_angular_parser(path)
-	if not path or type(path) ~= "string" then
-		return false
-	end
-	if not path:match("/src/app/") then
-		return false
-	end
-	return is_angular_project()
+  if not path or type(path) ~= "string" then
+    return false
+  end
+  if not path:match("/src/app/") then
+    return false
+  end
+  return is_angular_project()
 end
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-	pattern = { "*.component.html", "*.html" },
-	callback = function(args)
-		local buf = args.buf
-		local path = vim.api.nvim_buf_get_name(buf)
-		if should_use_angular_parser(path) then
-			local okay, _ = pcall(vim.treesitter.get_parser, buf, "angular")
-			if okay then
-				pcall(vim.treesitter.start, buf, "angular")
-			else
-				pcall(vim.treesitter.start, buf, "html")
-			end
-		end
-	end,
-	desc = "Apply Angular Treesitter parser or fallback",
+  pattern = { "*.component.html", "*.html" },
+  callback = function(args)
+    local buf = args.buf
+    local path = vim.api.nvim_buf_get_name(buf)
+    if should_use_angular_parser(path) then
+      local okay, _ = pcall(vim.treesitter.get_parser, buf, "angular")
+      if okay then
+        pcall(vim.treesitter.start, buf, "angular")
+      else
+        pcall(vim.treesitter.start, buf, "html")
+      end
+    end
+  end,
+  desc = "Apply Angular Treesitter parser or fallback",
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  desc = "Handle nvim-treesitter updates",
+  group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
+  callback = function(event)
+    if event.data.kind == "update" and event.data.spec.name == "nvim-treesitter" then
+      vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
+      ---@diagnostic disable-next-line: param-type-mismatch
+      local okay = pcall(vim.cmd, "TSUpdate")
+      if okay then
+        vim.notify("TSUpdate completed successfully!", vim.log.levels.INFO)
+      else
+        vim.notify("TSUpdate command not available yet, skipping", vim.log.levels.WARN)
+      end
+    end
+  end,
 })
